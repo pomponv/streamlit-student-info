14a1f73ab17535c269f0f70fb17f9254e8d6d4c1
pomponv <pomponv@pierre-fabre.com>
2024-05-30 12:22:42 +0200

    new version

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import streamlit as st
 
 # Title of the app
-st.title("Student Information")
+st.title("Faut-il encore travailler ?")
 prenom = st.text_input("Salut, moi c'est Charles, et toi comment t'appelles tu ?","ton_prénom")
 age = st.slider("Quel age as tu?", 0, 130, 25)
-st.write("tu as ", age, "ans, félicitation d'être arrivé jusque là !")
+if age < 16:
+    text = "return à l'ecole"
+elif age < 25:
+    text = "tu es sûr de ne pas vouloir continuer l'école"
+elif age < 30:
+    text="bon à un moment il faut travailler pour vivre et non vivre pour travailler !"
+elif age <70:
+    text = "aller encore un effort"
+elif age <80:
+    text = "un repot bien mérité"
+else :
+    text = "félicitation d'être arrivé jusque là !"
 if st.button("synthèse des infos transmises"):
-    st.write("Salut, ",prenom, "tu as ", age, "ans, félicitation d'être arrivé jusque là !")
+    st.write("Salut, ",prenom, "tu as ", age, "ans", text)
 else:
     st.write("au revoir")
